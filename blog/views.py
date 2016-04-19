082d56a80a0eefe0716a1822f303e07431ef4396
beomjun.gim3@gmail.com <beomjun.gim3@gmail.com>
2016-04-20 00:02:31 +0900

    first commit

---
+++ blog/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import PostForm
+
+# Create your views here.
+def index(request):
+    post_list = Post.objects.all()
+    return render(request, 'blog/index.html', {
+        'post_list' : post_list
+        })
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html',
+        {
+        'post' : post
+        })
+
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save()
+            return redirect('blog/post_detail.html', post.pk)
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_form.html', {
+        'form' : form
+        })
